be3b030c13545297cac105ec35ca963d836bda96
Albert Buenaventura <albert.buenaventura2009@gmail.com>
2020-02-25 19:09:55 +0800

    Added checker on singleton, updated readme

--- singleton/index.ts
+++ singleton/index.ts
@@ -4,5 +4,11 @@ const singletonClass1 = SingletonClass.getInstance();
 const singletonClass2 = SingletonClass.getInstance();
 
 if(singletonClass1 === singletonClass2) {
-    console.log('Single is now working')
+    console.log('Single is working')
+}
+//Increment counter on singletonClass1
+singletonClass1.increaseCounter()
+
+if(singletonClass2.getCounter() === singletonClass1.getCounter()) {
+    console.log('Singleton is working even changing attributes')
 }
